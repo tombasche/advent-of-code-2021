0fcfd17bd0adb04892672f0d3baae8d33b9adce0
Tom Basche <tcbasche@gmail.com>
2021-12-01 21:12:26 +0200

    Refactor part two

--- src/main/kotlin/dayOne/DayOne.kt
+++ src/main/kotlin/dayOne/DayOne.kt
@@ -11,13 +11,8 @@ fun findIncreases(items: List<Int>): Int = (0 until items.size - 1).foldIndexed(
     acc + if (isIncreasing(items[i], items[i + 1])) 1 else 0
 }
 
-fun findSlidingWindowIncreases(items: List<Int>): Int {
-    var counter = 0
-    for (i in 0 until items.size - 3) {
-        if (slidingWindow(items, i) < slidingWindow(items, i + 1))
-            counter++
-    }
-    return counter
+fun findSlidingWindowIncreases(items: List<Int>): Int = (0 until items.size - 3).foldIndexed(initial = 0) { i, acc, _ ->
+    acc + if (slidingWindow(items, i) < slidingWindow(items, i + 1)) 1 else 0
 }
 
 private fun isIncreasing(item: Int, nextItem: Int): Boolean = item < nextItem
