06592cc80db692b88e3dc2e16c80f1e196ed2229
Tom Basche <tcbasche@gmail.com>
2021-12-01 21:10:21 +0200

    Refactor into a fold

--- src/main/kotlin/dayOne/DayOne.kt
+++ src/main/kotlin/dayOne/DayOne.kt
@@ -7,23 +7,18 @@ typealias DayOneFunction = (items: List<Int>) -> Int
 fun dayOne(filename: String, f: DayOneFunction = ::findIncreases): Int = f(readFile(filename).map { it.toInt() })
 
 
-fun findIncreases(items: List<Int>): Int  {
-    var counter = 0
-    for (i in 0 until items.size - 1) {
-        if (isIncreasing(items[i], items[i+1]))
-            counter ++
-    }
-    return counter
+fun findIncreases(items: List<Int>): Int = (0 until items.size - 1).foldIndexed(initial = 0) { i, acc, _ ->
+    acc + if (isIncreasing(items[i], items[i + 1])) 1 else 0
 }
 
 fun findSlidingWindowIncreases(items: List<Int>): Int {
     var counter = 0
     for (i in 0 until items.size - 3) {
-        if (slidingWindow(items, i) < slidingWindow(items, i+1))
-            counter ++
+        if (slidingWindow(items, i) < slidingWindow(items, i + 1))
+            counter++
     }
     return counter
 }
 
-fun isIncreasing(item: Int, nextItem: Int): Boolean = item < nextItem
-fun slidingWindow(items: List<Int>, i: Int): Int = items[i] + items[i+1] + items[i+2]
+private fun isIncreasing(item: Int, nextItem: Int): Boolean = item < nextItem
+private fun slidingWindow(items: List<Int>, i: Int): Int = items[i] + items[i + 1] + items[i + 2]
